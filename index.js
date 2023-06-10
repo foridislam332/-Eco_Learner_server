c6f77d4c21ac7b852377ffea22c5200e1361a313
foridislam332 <foridislam991@gmail.com>
2023-06-10 21:00:35 +0600

    send feedback done

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -129,6 +129,20 @@ async function run() {
                 }
             };
             const result = await classesCollection.updateOne(filter, updateDoc, option);
+            res.send(result)
+        });
+
+        // send feedback
+        app.patch('/manageClasses/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const feedback = req.query.feedback;
+            const updateDoc = {
+                $set: {
+                    status: feedback,
+                }
+            };
+            const result = await classesCollection.updateOne(filter, updateDoc);
             res.send(result)
         });
 
